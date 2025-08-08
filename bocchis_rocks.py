c8a95c99920069334a20f8eeae7f37e9843d67e2
FaisalDev <111685925+FaisalDev09@users.noreply.github.com>
2025-08-08 17:04:24 +0200

    Add files via upload

---
+++ bocchis_rocks.py
@@ -0,0 +1,29 @@
+n = int(input())
+
+vikter = list(map(int, input().split()))
+
+if len(vikter) != n:
+    print("fel")
+    exit()
+sorterade_vikter = sorted(vikter)
+resultat = {}
+for i , vikt in enumerate(sorterade_vikter):
+    resultat [vikt]  = i
+
+for v in vikter:
+    print(resultat[v], end=' ')
+    
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+        
