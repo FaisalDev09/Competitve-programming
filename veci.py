4b05aa4cfcedaee1176d6e6c71f08eb401186954
FaisalDev <111685925+FaisalDev09@users.noreply.github.com>
2025-08-31 17:36:32 +0200

    Add files via upload

--- veci.py
+++ veci.py
@@ -1,10 +1,26 @@
 nums = list(map(int, input()))
+n = len(nums)
 
-if nums == sorted(nums, reverse=True):
-    print("0")
+# Step 1: Find the pivot
+# S tart from the second last element, the last element is at i+1
+i = n - 2
+while i >= 0 and nums[i] >= nums[i+1]:
+    i -= 1  # Move one step to the left
+
+if i < 0:
+    # If no pivot found, the list is in descending order
+    # This means we are at the last permutation
+    print(0)  # Optional: print 0 to indicate there was no next permutation
 else:
-    i = len(nums) - 2
-    while i >= 0 and nums[i] >= nums[i+1]:
-        i-= 1
-    
-    
+    # Step 2: Find the smallest element in the suffix that is larger than the pivot
+    j = len(nums) - 1  # Start from the last element
+    while nums[j] <= nums[i]:
+        j -= 1  # Move left until we find a number bigger than pivot
+
+    # Step 3: Swap pivot with that element
+    nums[i], nums[j] = nums[j], nums[i]
+
+    # Step 4: Reverse the suffix (elements to the right of pivot)
+    nums[i+1:] = reversed(nums[i+1:])
+
+    print(*nums, sep="")
