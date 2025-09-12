3fe3d3526938614816de22dcfb604056c125de29
FaisalDev <111685925+FaisalDev09@users.noreply.github.com>
2025-09-12 23:36:26 +0200

    Add files via upload

--- missing_gnomes.py
+++ missing_gnomes.py
@@ -1,15 +1,33 @@
 
-n, m = map(int,input().split())
+n, m = map(int,input().split()) # n - total number of gnomes , m - number of gnomes which is left 
 
-sekvens = []
+sekvens = [] # contains the left gnomes
 gnomes = set(range(1,n+1))
 for _ in range(m):
     num = int(input())
     sekvens.append(num)
 
-missing = sorted(gnomes - set(sekvens))
+missing = sorted(gnomes - set(sekvens)) # contain the missing gnomes (set - set)
 
-for i in sekvens: 
+result = [] # final result 
+j = 0 # index för missing (to not use two loop)
+i = 0 # index för sekvens
+
+for _ in range(n): # The total number of gnomes is n 
+    # Decide whetehr to take the next gnome from sekvens or missing
+    if i <m and (j >= len(missing) or sekvens[i] < missing[j]):
+        result.append(sekvens[i])
+        i +=1
+    else:
+        result.append(missing[j])
+        j +=1
+
+for g in result:
+    print(g)
+
+
+
+
     
 
 
